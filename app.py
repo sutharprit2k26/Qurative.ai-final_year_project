0aaced462560e5bddbfd2f51566541bdfd1d8b6f
sutharprit2k26 <pritsuthar.dev@gmail.com>
2021-12-07 21:31:44 +0530

    template

---
+++ app.py
@@ -0,0 +1,14 @@
+from jinja2 import Template
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
